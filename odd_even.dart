563400a46d1f64ce0923195471a0293f9a043d90
Karthika0806 <96553632+Karthika0806@users.noreply.github.com>
2022-02-19 13:30:53 +0530

    Add files via upload

---
+++ odd_even.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+void main() {
+  var num = 18;
+  if (num % 2 == 0) {
+    print("number ${num} is a even number");
+  } else {
+    print("number ${num} is a odd number");
+  }
+}
